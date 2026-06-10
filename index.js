b956a55bfcf295afab4a3c6bbdedc0deab021633
Sett Sarverott <sett@sarverott.com>
2026-06-10 12:49:44 +0200

    fixing by replacing asserw with... with

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // on Apache license 2.0 @ IPI DAO 2026
 // maintainership by Sett Sarverott
 
-import chainConfiguration from "./config.json" assert {type: 'json'};
+import chainConfiguration from "./config.json" with {type: 'json'};
 
 export default chainConfiguration;
